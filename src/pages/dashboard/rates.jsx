53ef9e58a8baf0aea0af08c48c9dc15d4ab3481d
Vladislav <timaevvladislav@gmail.com>
2023-01-21 04:54:50 +0600

    add:rates

--- src/pages/dashboard/rates.jsx
+++ src/pages/dashboard/rates.jsx
@@ -10,8 +10,22 @@ import team from "/public/icons/main-icons/team.svg"
 import select from "/public/icons/select.svg"
 import routes from "@/routes.jsx"
 
-export function Rates() {
+const RateIcons = ({icon, title}) => (
+  <span className="flex items-center  justify-center rounded-[5px] bg-[#1F232C] inline-block px-2 py-3 leading-none text-white font-semibold text-xs">
+  <img src={icon} alt={title} className="pr-1 h-[15px]" height="13px" width="22px" />
+    {title}
+  </span>
+)
 
+const RateButton = () => (
+  <div className="flex justify-center">
+    <button type="button" className="border-solid border-2 border-white rounded-[34px] text-white hover:bg-[#15A655] hover:border-0 font-medium rounded-1 text-sm px-4 py-1 text-center inline-flex items-center">
+      Оплатить
+    </button>
+  </div>
+)
+
+export function Rates() {
   return (
    <div className="flex">
      <Sidenav routes={routes} brandImg="/logo.png" />
@@ -24,10 +38,7 @@ export function Rates() {
              <div className="w-full sm:w-1/2 md:w-1/2 xl:w-1/5 p-4">
                <div className="h-[402px] c-card block bg-[#131621] shadow-md hover:shadow-xl rounded-lg overflow-hidden">
                  <div className="p-4">
-                <span className="flex items-center  justify-center rounded-[5px] bg-[#1F232C] inline-block px-2 py-4 leading-none text-white font-semibold text-xs">
-                  <img src={free} alt="icon" className="pr-1" />
-                  Free
-                </span>
+                   <RateIcons icon={free} title="Free" />
                    <h2 className="mt-2 mb-2 text-white font-bold text-center text-[12px]">
                      Весь функционал
                    </h2>
@@ -43,21 +54,14 @@ export function Rates() {
                  <h2 className="text-[30px] text-base text-white font-bold text-center">
                    $0
                  </h2>
-                 <div className="flex justify-center">
-                   <button type="button" className="border-solid border-2 border-white rounded-[34px] text-white hover:bg-[#15A655] hover:border-0 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-1 text-sm px-4 py-1 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 ">
-                     Оплатить
-                   </button>
-                 </div>
+                 <RateButton />
                </div>
              </div>
 
              <div className="w-full sm:w-1/2 md:w-1/2 xl:w-1/5 p-4">
                <div className="h-[402px] c-card block bg-[#131621] shadow-md hover:shadow-xl rounded-lg overflow-hidden">
                  <div className="p-4">
-                <span className="flex items-center justify-center rounded-[5px] bg-[#1F232C] inline-block px-2 py-3 leading-none text-white font-semibold text-xs">
-                  <img src={micro} alt="icon" className="pr-1" />
-                  Micro
-                </span>
+                   <RateIcons icon={micro} title="Micro" />
                    <h2 className="mt-2 text-white font-bold text-center text-[12px]">
                      Весь функционал
                    </h2>
@@ -77,21 +81,14 @@ export function Rates() {
                  <h2 className="text-[30px] pt-[17px] pb-[17px] text-base text-white font-bold text-center">
                    $29
                  </h2>
-                 <div className="flex justify-center">
-                   <button type="button" className="border-solid border-2 border-white rounded-[34px] text-white hover:bg-[#15A655] hover:border-0 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-1 text-sm px-4 py-1 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 ">
-                     Оплатить
-                   </button>
-                 </div>
+                 <RateButton />
                </div>
              </div>
 
              <div className="w-full sm:w-1/2 md:w-1/2 xl:w-1/5 p-4">
                <div className="h-[402px] c-card block bg-[#131621] shadow-md hover:shadow-xl rounded-lg overflow-hidden">
                  <div className="p-4">
-                <span className="flex items-center justify-center rounded-[5px] bg-[#1F232C] inline-block px-2 py-3 leading-none text-white font-semibold text-xs">
-                  <img src={base} alt="icon" className="pr-1" />
-                  Base
-                </span>
+                   <RateIcons icon={base} title="Base" />
                    <h2 className="mt-2 text-white font-bold text-center text-[12px]">
                      Весь функционал
                    </h2>
@@ -110,20 +107,13 @@ export function Rates() {
                  <h2 className="text-[30px] pt-[17px] pb-[17px] text-base text-white font-bold text-center">
                    $79
                  </h2>
-                 <div className="flex justify-center">
-                   <button type="button" className="border-solid border-2 border-white rounded-[34px] text-white hover:bg-[#15A655] hover:border-0 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-1 text-sm px-4 py-1 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 ">
-                     Оплатить
-                   </button>
-                 </div>
+                 <RateButton />
                </div>
              </div>
              <div className="w-full sm:w-1/2 md:w-1/2 xl:w-1/5 p-4">
                <div className="h-[402px] c-card block bg-[#131621]  shadow-md hover:shadow-xl rounded-lg overflow-hidden">
                  <div className="p-4">
-                <span className="flex items-center justify-center rounded-[5px] bg-[#1F232C] inline-block px-2 py-3 leading-none text-white font-semibold text-xs">
-                  <img src={middle} alt="icon" className="pr-1" />
-                  Middle
-                </span>
+                 <RateIcons icon={middle} title="Middle" />
                    <h2 className="mt-2 text-white font-bold text-center text-[12px]">
                      Весь функционал
                    </h2>
@@ -154,21 +144,14 @@ export function Rates() {
                  <h2 className="text-[30px] pt-[17px] pb-[17px] text-base text-white font-bold text-center">
                    $199
                  </h2>
-                 <div className="flex justify-center">
-                   <button type="button" className="border-solid border-2 border-white rounded-[34px] text-white hover:bg-[#15A655] hover:border-0 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-1 text-sm px-4 py-1 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 ">
-                     Оплатить
-                   </button>
-                 </div>
+                 <RateButton />
                </div>
              </div>
 
              <div className="w-full sm:w-1/2 md:w-1/2 xl:w-1/5 p-4">
                <div className="h-[402px] c-card block bg-[#131621] shadow-md hover:shadow-xl rounded-lg overflow-hidden">
                  <div className="p-4">
-               <span className="flex items-center justify-center rounded-[5px] bg-[#1F232C] inline-block px-2 py-4 leading-none text-white font-semibold text-xs">
-                 <img src={team} alt="icon" className="pr-1" />
-                 Team
-                </span>
+                 <RateIcons icon={team} title="Team" />
                    <h2 className="mt-2 text-white font-bold text-center text-[12px]">
                      Весь функционал
                    </h2>
@@ -196,11 +179,7 @@ export function Rates() {
                  <h2 className="text-[30px] pt-[17px] pb-[17px] text-base text-white font-bold text-center">
                    $399
                  </h2>
-                 <div className="flex justify-center">
-                   <button type="button" className="border-solid border-2 border-white rounded-[34px] text-white hover:bg-[#15A655] hover:border-0 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-1 text-sm px-4 py-1 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 ">
-                     Оплатить
-                   </button>
-                 </div>
+                 <RateButton />
                </div>
              </div>
            </div>
