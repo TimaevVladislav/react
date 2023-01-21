603c49e1230a720eb4a605bf03b8425ead7c07cc
Vladislav <timaevvladislav@gmail.com>
2023-01-22 01:18:05 +0600

    add:range

--- src/components/Range.jsx
+++ src/components/Range.jsx
@@ -9,9 +9,9 @@ const ProgressBar = () => (
 )
 export const Range = () => (
   <div>
-    <div className="flex justify-between w-[170px] items-center text-white font-bold ml-[40px] rounded-[5px] inline-block px-2 py-2 leading-none text-white ">
+    <div className="flex justify-between w-[170px] items-center text-white font-bold ml-[40px] rounded-[5px] px-2 py-2 leading-none text-white ">
       <div className="items-center w-[160px]">
-        <h1 className="text-[10px]">Лицензии до:</h1>
+        <h1 className="text-[10px] normal-case">Лицензии до:</h1>
       </div>
       <div className="items-center">
         <p className="text-white text-[12px]">02.03.23</p>
