ad9e9e86d722d248f2d07a0f342a92f3c101087b
Vladislav <timaevvladislav@gmail.com>
2023-01-22 08:03:52 +0600

    add:card:style

--- src/components/Avatars.jsx
+++ src/components/Avatars.jsx
@@ -11,3 +11,11 @@ export const AvatarNavbar = ({title}) => (
   </div>
 )
 
+export const AvatarSettings = ({title}) => (
+  <div>
+    <img className="object-cover w-16 h-16 border-2 border-[#31373F] rounded-full h-[43px] w-[40px]"src={avatar} alt="Avatar" />
+    <p className="text-[#15A655] text-[11px] text-center font-bold pt-1">
+      {title}
+    </p>
+  </div>
+)
--- src/components/CardRefferral.jsx
+++ src/components/CardRefferral.jsx
@@ -9,19 +9,19 @@ export const CardRefferral = () => {
      <div className="relative overflow-x-auto sm:rounded-lg">
        <Heading heading="Реферальнная система" />
        <div className="h-[163px] c-card block bg-[#1A1E2D] rounded-lg overflow-hidden">
-         <div className="p-4 flex justify-between">
-           <div className="m-auto col-2">
-             <img src={refferralCard} alt="icon" className="pr-1" />
+         <div className="flex">
+           <div className="w-[120px] m-auto">
+             <img src={refferralCard} alt="icon" className="pl-[25px] pb-[91px]" />
            </div>
-          <div>
-            <p className="items-center text-[14px] rounded-[5px] inline-block px-2 py-4 leading-none text-white font-semibold text-xs">
-              Вы получите <span className="text-[#15A655] pl-1 pr-1">15% от всех платежей</span> приведенных вами пользователей пожизненно.
-            </p>
-            <p className="text-[#919EAB] items-center rounded-[5px] inline-block px-2 leading-none text-white font-semibold text-xs">
-              Для вывода реферальных средств, свяжитесь с оператором
-            </p>
-            <Inputs />
-          </div>
+           <div className="col-4">
+             <p className="items-center text-[14px] rounded-[5px] inline-block py-4 leading-none text-white font-semibold text-xs">
+               Вы получите <span className="text-[#15A655] pl-1 pr-1">15% от всех платежей</span> приведенных вами пользователей пожизненно.
+             </p>
+             <p className="text-[#919EAB] items-center rounded-[5px] inline-block px-2 leading-none text-white font-semibold text-xs">
+               Для вывода реферальных средств, свяжитесь с оператором
+             </p>
+             <Inputs />
+           </div>
          </div>
        </div>
      </div>
@@ -29,13 +29,12 @@ export const CardRefferral = () => {
   )
 }
 
-export const CardStatistics = ({ heading }) => {
+export const CardStatistics = () => {
   return (
-    <div className="relative overflow-x-auto sm:rounded-lg">
+    <div className="ml-[30px] relative overflow-x-auto sm:rounded-lg">
       <Heading heading="Ваша статистика" />
       <div className="h-[163px] c-card block bg-[#1A1E2D]  rounded-lg overflow-hidden">
-        <div className="p-4">
-          <div className="flex justify-between items-center pl-4 rounded-[5px] rounded-b-[0] bg-[#1F232C] inline-block py-2 leading-none text-white font-semibold text-xs">
+          <div className="h-[81px] flex justify-between items-center pl-4 rounded-[5px] rounded-b-[0] bg-[#1F232C] inline-block py-2 leading-none text-white font-semibold text-xs">
             <div className="items-center w-[321.75px] text-center">
               <h1 className="font-bold text-[30px]">1544</h1>
             </div>
@@ -43,14 +42,13 @@ export const CardStatistics = ({ heading }) => {
               <h1 className="font-bold text-[13px]">Всего зарегистрировано пользователей по вашей реферальной ссылке</h1>
             </div>
           </div>
-          <div className="flex justify-between items-center border-[#242937] border-t rounded-b-[5px] bg-[#1F232C] inline-block py-2 leading-none text-white font-semibold text-xs">
+          <div className="h-[81px] flex justify-between items-center border-[#242937] border-t rounded-b-[5px] bg-[#1F232C] inline-block py-2 leading-none text-white font-semibold text-xs">
             <div className="items-center w-[321.75px] text-center">
               <h1 className="font-bold text-[16px]">Баланс:</h1>
             </div>
             <div className="items-center w-[350.75px]">
               <h1 className="font-bold text-[30px] text-[#15A655] pr-[90px]">$1544</h1>
             </div>
-          </div>
         </div>
       </div>
     </div>
